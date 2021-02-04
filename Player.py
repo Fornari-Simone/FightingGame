34cadbec2f46861f59601f733bbbb7930d758201
Fornari-Simone <65810491+Fornari-Simone@users.noreply.github.com>
2021-02-04 14:25:03 +0100

    add movement

--- Player.py
+++ Player.py
@@ -1,13 +1,40 @@
 from typing import Tuple
 from pygame.draw import rect
+
+GRAVITY = 9.8
+VEL_Y = 0
+
 class Player:
-  def __init__(self, screen, vel: x_vel: int, y_vel: int x: int, y: int, width: int, height: int, 
-    color: Tuple[int, int, int]):
-    self.gravity = 9.8
-    self.vel = (x_vel, y_vel)
+  def __init__(self, screen, x_vel: int, y_vel: int, width: int, height: int, color: Tuple[int, int, int]):
+    self.screen = screen
+    self.vel_x, self.vel_y = (x_vel, y_vel)
+    self.mov_x, self.mov_y = (0, 0)
+    VEL_Y = self.vel_y
     self.color = color
-    self.rect = rect(screen, color, [x, y, width, height])
+    self.rect = rect(screen, color, [0, 550, width, height])
     #self.sprite = load(pathImage)
 
-  def draw(screen):
-    
+  def draw(self):
+    rect(self.screen, self.color, self.rect)
+
+  def move_x(self):
+    self.rect = self.rect.move(self.vel_x * self.mov_x, 0)
+  
+  def move_y(self):
+    if self.mov_y == 0: return
+    _, y_sc = self.screen.get_size()
+    self.vel_y -= (self.mov_y * GRAVITY)
+    print(self.vel_y)
+    if self.rect.bottom - self.vel_y > y_sc: 
+      self.mov_y = 0
+      self.vel_y = VEL_Y
+      self.rect.move_ip(0, y_sc - self.rect.height)
+      return
+    self.rect = self.rect.move(0, -self.vel_y)
+    # print(y_sc)
+    # if y_sc < self.rect.bottom: 
+
+    #   x = self.rect.x
+    #   y = y_sc - self.rect.height
+    #   self.rect.move_ip(x, y)
+    # print(f"{self.rect.x} {self.rect.y}")
--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-from pygame import init, QUIT
+from pygame import init, KEYDOWN, KEYUP, K_LEFT, K_a, K_RIGHT, K_d, K_UP, K_w, QUIT
 from pygame.display import set_caption, set_icon, set_mode, update
 from pygame.draw import rect
 from pygame.event import get
 from pygame.image import load
-#from Player import Player
+import pygame
+from Player import Player
 
 # WIDTH, HEIGHT
-SIZE = (800, 600)
+SIZE = (600, 600)
 # RED, GREEN, BLUE
 COLOR = (0, 0, 0)
 TITLE = "Fighting Game"
 ICON_PATH = "img/GameIcon.png"
 
-#pl = Player((0, 0),(0,0,50,50), (255, 255, 255))
 init()
 
 screen = set_mode(SIZE)
 set_caption(TITLE)
 set_icon(load(ICON_PATH))
-rct = rect(screen, (255,255,255), [0,0,50,50])
+
+pl = Player(screen, 1, 15, 50, 50, (255, 255, 255))
 run = True
 while run:
   for event in get():
     if event.type == QUIT: run = False
+    elif event.type == KEYDOWN:
+      if event.key == K_a or event.key == K_LEFT:
+        pl.mov_x = -1
+      elif event.key == K_d or event.key == K_RIGHT:  
+        pl.mov_x = 1
+      elif event.key == K_w or event.key == K_UP:
+        pl.mov_y = 0.1
+    elif event.type == KEYUP:
+      if (event.key == K_a or 
+          event.key == K_LEFT or
+          event.key == K_d or
+          event.key == K_RIGHT):
+        pl.mov_x = 0
   
   screen.fill(COLOR)
-  rct = rect(screen, (255,255,255), rct)
+  pl.move_x()
+  pl.move_y()
+  pl.draw()
+  #pygame.Rect.move_ip(pl.rect, 0, 50)
   update()
